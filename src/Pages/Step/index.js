bb92626ebd1fdc1c184d28825c24a834e30eaa3a
allandallopez <allandello22@gmail.com>
2021-09-12 02:29:57 +0700

    add section step information mobile

--- src/Pages/Step/index.js
+++ src/Pages/Step/index.js
@@ -1,62 +1,73 @@
-import Illustration from "@Image/illustration.png";
+import Illustration from '@Image/illustration.png'
 
 export default function Step() {
-  return (
-    <section className="step">
-      <h3>
-        <span className="clr-primaryDark">Easy as </span>
-        <span className="clr-primary">one</span>
-        <span className="clr-primaryDark">-</span>
-        <span className="clr-orange">two</span>
-        <span className="clr-primaryDark">-</span>
-        <span className="clr-red">three</span>
-        <span className="clr-primaryDark">...</span>
-      </h3>
-      <div className="step__information">
-        <div className="step__num blue">1</div>
-        <div>
-          <h5 className="clr-primaryDark">First Step</h5>
-          <p>
-            This is a content of the steps. Can be up to three lines. This is a
-            content of the steps. Can be up to three lines. This is a content of
-            the steps. Can be up to three lines. This is a content of the steps.
-            Can be up to three lines. This is a content of the steps. Can be up
-            to three lines. This is a content of the steps. Can be up to three
-            lines.
-          </p>
-        </div>
-        <div className="step__num orange">2</div>
-        <div>
-          <div>
-            <h5 className="clr-primaryDark">Second Step</h5>
-            <p>
-              This is a content of the steps. Can be up to three lines. This is
-              a content of the steps. Can be up to three lines. This is a
-              content of the steps. Can be up to three lines. This is a content
-              of the steps. Can be up to three lines.
-            </p>
-          </div>
-          <div className="flex illustration">
-            <img src={Illustration} alt="illustration" />
-            <div>
-              <h5 className="clr-primaryDark">Illustration</h5>
-              <p>This is a content of the steps. Can be up to three lines. </p>
+   return (
+      <section className='step'>
+         <h3>
+            <span className='clr-primaryDark'>Easy as </span>
+            <span className='clr-primary'>one</span>
+            <span className='clr-primaryDark'>,</span>
+            <span className='clr-orange'>two</span>
+            <span className='clr-primaryDark'>,</span>
+            <span className='clr-red'>three</span>
+            <span className='clr-primaryDark'>...</span>
+         </h3>
+         <div className='step__information'>
+            <div className='top__body_tag'>
+               <div className='step__num_mobile'>1</div>
+               <div className='top__body__title'>First Step</div>
             </div>
-          </div>
-        </div>
-        <div className="step__num red">3</div>
-        <div>
-          <h5 className="clr-primaryDark">Third Step</h5>
-          <p>
-            This is a content of the steps. Can be up to three lines. This is a
-            content of the steps. Can be up to three lines. This is a content of
-            the steps. Can be up to three lines.
-          </p>
-        </div>
-      </div>
-      <button className="step__btn btn btn__primary clr-white">
-        Learn More
-      </button>
-    </section>
-  );
+            {/* SEPAREATE COMPONENT MOBILE VIIEW */}
+            <div className='step__num blue'>1</div>
+            <div>
+               <h5 className='clr-primaryDark'>First Step</h5>
+               <p>
+                  This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up to
+                  three lines. This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up to three lines. This is a content of the steps.
+                  Can be up to three lines.
+               </p>
+            </div>
+
+            <div className='top__body_tag'>
+               <div className='step__num_mobile'>2</div>
+               <div className='top__body__title'>Second Step</div>
+            </div>
+            {/* SEPAREATE COMPONENT MOBILE VIIEW */}
+            <div className='step__num orange'>2</div>
+            <div>
+               <div>
+                  <h5 className='clr-primaryDark'>Second Step</h5>
+                  <p>
+                     This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up
+                     to three lines. This is a content of the steps. Can be up to three lines.
+                  </p>
+               </div>
+               <div className='flex illustration'>
+                  <img src={Illustration} alt='illustration' />
+                  <div className='illustration title'>Illustration</div>
+
+                  <div>
+                     <h5 className='clr-primaryDark'>Illustration</h5>
+                     <p>This is a content of the steps. Can be up to three lines. </p>
+                  </div>
+               </div>
+            </div>
+
+            <div className='top__body_tag'>
+               <div className='step__num_mobile'>3</div>
+               <div className='top__body__title'>Third Step</div>
+            </div>
+            {/* SEPAREATE COMPONENT MOBILE VIIEW */}
+            <div className='step__num red'>3</div>
+            <div>
+               <h5 className='clr-primaryDark'>Third Step</h5>
+               <p>
+                  This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up to three lines. This is a content of the steps. Can be up to
+                  three lines.
+               </p>
+            </div>
+         </div>
+         <button className='step__btn btn btn__primary clr-white'>Learn More</button>
+      </section>
+   )
 }
